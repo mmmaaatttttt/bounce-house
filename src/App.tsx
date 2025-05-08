061f557613b0bd1d320b0b2c586e49459cdf5a2a
Matt Lane <3916770+mmmaaatttttt@users.noreply.github.com>
2025-05-07 21:06:25 -0700

    update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import InteractiveBase from "./components/interactive/InteractiveBase";
+import InteractiveControls from "./components/interactive-controls/InteractiveControls";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+    <div className="app-container">
+      <header className="header">
+        <h1 className="main-title">Bounce House</h1>
+        <p className="subtitle">
+          Mirror, mirror, on the wall. And that wall. And maybe the other wall
+          too.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </header>
+
+      <InteractiveControls />
+      <main className="main-content">
+        <div className="interactive-container">
+          <InteractiveBase />
+        </div>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
